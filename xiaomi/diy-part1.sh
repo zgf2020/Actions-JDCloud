62bddf48091b172400a533e5c0f1f0a701a8d059
Microyin823 <151486794+Microyin823@users.noreply.github.com>
2024-06-14 20:20:20 +0800

    Update diy-part1.sh

--- xiaomi/diy-part1.sh
+++ xiaomi/diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 git clone https://github.com/kenzok8/openwrt-packages package/kenzo
 git clone https://github.com/kenzok8/small package/small
-git clone https://github.com/qosmio/nss-packages.git package/nss
+git clone https://github.com/qosmio/nss-packages.git package/nss-packages
