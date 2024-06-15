d6ba2d3b54892ed8b4ed1c56cc0f4ab9546cce03
Microyin823 <151486794+Microyin823@users.noreply.github.com>
2024-06-15 20:05:34 +0800

    Update diy-part2.sh

--- MT7981/diy-part2.sh
+++ MT7981/diy-part2.sh
@@ -43,3 +43,11 @@ sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 #do
 #	[ -n "$(grep "upx" "$a")" ] && sed -i "/upx/d" "$a"
 #done
+
+# 修改本地时间格式
+sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
+
+# 修改版本为编译日期
+date_version=$(date +"%y.%m.%d")
+orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+sed -i "s/${orig_version}/R${date_version} by Haiibo/g" package/lean/default-settings/files/zzz-default-settings
