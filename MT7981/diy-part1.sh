6ae67ee55a7d3d6044573d53986dd459fd1d15fb
Microyin823 <151486794+Microyin823@users.noreply.github.com>
2024-06-15 23:27:14 +0800

    Update diy-part1.sh

--- MT7981/diy-part1.sh
+++ MT7981/diy-part1.sh
@@ -9,3 +9,6 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
+# Add a feed source
+git clone https://github.com/kenzok8/openwrt-packages package/kenzo
+git clone https://github.com/kenzok8/small package/small
